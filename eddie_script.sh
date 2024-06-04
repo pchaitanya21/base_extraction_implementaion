c9bfa3ced100379aa78e95c3542987a6503a4def
dmcgrath19 <s2558433@ed.ac.uk>
2024-06-04 18:04:55 +0100

    fix eddie script

--- eddie_script.sh
+++ eddie_script.sh
@@ -1,33 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=base_extraction
-#SBATCH --output=/exports/eddie/scratch/s2558433/base_extraction_%j.log
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=16G
-#SBATCH --time=02:00:00
-#SBATCH --partition=general
+#$ -N base_extraction
+#$ -o /exports/eddie/scratch/s2558433/base_extraction_$JOB_ID.log
+#$ -e /exports/eddie/scratch/s2558433/base_extraction_$JOB_ID.err
+#$ -cwd
+#$ -pe sharedmem 16
+#$ -l h_vmem=4G
+#$ -l h_rt=12:00:00
 
-# Define variables
-SCRATCH_DIR="/exports/eddie/scratch/s2558433"
-REPO_URL="https://github.com/pchaitanya21/base_extraction_implementation.git"
-PROJECT_DIR="$SCRATCH_DIR/base_extraction_implementation"
-CONDA_DIR="$SCRATCH_DIR/miniconda3"
+# Create /activate conda env if it doesn't exist
+source /exports/eddie/scratch/s2558433/miniconda3/etc/profile.d/conda.sh
 
-# Load Conda environment
-source $CONDA_DIR/etc/profile.d/conda.sh
+cd /exports/eddie/scratch/s2558433/base_extraction_implementaion
 
-# Clone the repository
-git clone $REPO_URL $PROJECT_DIR
+conda activate extract
 
-# Create a new Conda environment and install dependencies
-conda create --prefix $SCRATCH_DIR/extract python=3.8 -y
-conda activate $SCRATCH_DIR/extract
-pip install -r $PROJECT_DIR/requirements.txt
+pip install -r requirements.txt
 
-# Change to the project directory
-cd $PROJECT_DIR
-
-# Run the Python script
+# Run the main script
 python main.py --N 1000 --batch-size 10 --model1 EleutherAI/pythia-2.8b --model2 EleutherAI/pythia-160m --corpus-path monology/pile-uncopyrighted
-
-conda deactivate
