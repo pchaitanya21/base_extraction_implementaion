46193c986456ef0df4947254b961560d1cdd6452
Chaitanya Pandey <chaitanya.p2001@gmail.com>
2024-06-06 12:07:17 +0100

    print statement

--- main_load.py
+++ main_load.py
@@ -16,7 +16,7 @@ def main(args):
     path="swa_sample.txt"
     ds= parse_swahili(path)
     print("Length:", len(ds))
-
+    print("Sanity check of data:",ds[:200])
     seq_len = 256
     top_k = 40
 
