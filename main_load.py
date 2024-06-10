4f402724a877bf1f298fc19adfe6d274d733fece
Chaitanya Pandey <chaitanya.p2001@gmail.com>
2024-06-10 22:56:04 +0100

    reverted serengeti changes

--- main_load.py
+++ main_load.py
@@ -5,8 +5,7 @@ import torch
 import zlib
 import csv
 from datasets import load_dataset
-from transformers import GPTNeoXForCausalLM, AutoTokenizer, AutoModelForMaskedLM
-
+from transformers import GPTNeoXForCausalLM, AutoTokenizer
 from tqdm import tqdm
 from model_utils import calculate_perplexity, print_best, device
 from extraction import parse_pilecorpus, parse_swahili
@@ -28,9 +27,9 @@ def main(args):
     tokenizer.padding_side = "left"
     tokenizer.pad_token = tokenizer.eos_token
 
-    model1 = AutoModelForMaskedLM.from_pretrained(args.model1, return_dict=True).to(device)
+    model1 = GPTNeoXForCausalLM.from_pretrained(args.model1, return_dict=True).to(device)
     model1.config.pad_token_id = model1.config.eos_token_id
-    model2 = AutoModelForMaskedLM.from_pretrained(args.model2, return_dict=True).to(device)
+    model2 = GPTNeoXForCausalLM.from_pretrained(args.model2, return_dict=True).to(device)
     model2.eval()
 
     samples = []
