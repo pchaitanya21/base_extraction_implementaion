7501ef3fcb41bbbd014f79c1a65e45b5c5b76493
Chaitanya Pandey <chaitanya.p2001@gmail.com>
2024-06-13 14:34:31 +0100

    minor buigs

--- main_load.py
+++ main_load.py
@@ -115,6 +115,7 @@ def main(args):
 
     model1_name = args.model1.replace("/", "_")
     model2_name = args.model2.replace("/", "_")
+    
     comparison_result = [1 if sample == prompt else 0 for sample, prompt in zip(samples, prompt_suff)]
     ones_count = sum(comparison_result)
     total_count = len(comparison_result)
--- model_utils.py
+++ model_utils.py
@@ -6,35 +6,7 @@ from pprint import pprint, pformat
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-class DecayingTemperatureWarper(LogitsProcessor):
-    def __init__(self, temperature: float):
-        if not isinstance(temperature, float) or not (temperature > 0):
-            raise ValueError(f"`temperature` has to be a strictly positive float, but is {temperature}")
 
-        self.temperature = temperature
-        self.mapping = {1: 10.0, 2: 9.53, 3: 9.06, 4: 8.59, 5: 8.12, 6: 7.65, 7: 7.18, 8: 6.71, 9: 6.24, 10: 5.77, 11: 5.30, 
-                        12: 4.83, 13: 4.36, 14: 3.89, 15: 3.42, 16: 2.95, 17: 2.49, 18: 2.01, 19: 1.54, 20: 1.0}
-
-    def __call__(self, input_ids: torch.Tensor, scores: torch.Tensor) -> torch.FloatTensor:
-        cur_len = input_ids.shape[-1]
-        self.temperature = self.mapping.get(cur_len, 1.0)
-        
-        return scores
-
-
-def calculate_perplexity_sliding(input_sentence, model, tokenizer, device, window_size=50):
-    """
-    Calculate min(exp(loss)) over a sliding window
-    """
-    tokenized = tokenizer(input_sentence)
-    input = torch.tensor(tokenized.input_ids).to(device)
-    min_perplexity = 100000
-    with torch.no_grad():
-        for start_idx in range(input.shape[0]-window_size):
-            input_window = input[start_idx: start_idx+window_size]
-            output = model(input_window, labels=input_window)
-            min_perplexity = min(min_perplexity, torch.exp(output.loss))
-    return min_perplexity
 def calculate_perplexity(sentence, model, tokenizer):
     input_ids = torch.tensor(tokenizer.encode(sentence)).unsqueeze(0)
     input_ids = input_ids.to(device)
