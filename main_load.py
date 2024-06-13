d1078de909efaf0eeba87d88eb68d58706c17934
Chaitanya Pandey <chaitanya.p2001@gmail.com>
2024-06-13 12:54:37 +0100

    added temperature and sliding window

--- main_load.py
+++ main_load.py
@@ -4,13 +4,42 @@ import sys
 import torch
 import zlib
 import csv
+import pandas as pd
 from datasets import load_dataset
 from transformers import GPTNeoXForCausalLM, AutoTokenizer
+from transformers.generation_logits_process import LogitsProcessor, LogitsProcessorList
 from tqdm import tqdm
 from model_utils import calculate_perplexity, print_best, device
 from extraction import parse_pilecorpus, parse_swahili
 
+class DecayingTemperatureWarper(LogitsProcessor):
+    def __init__(self, temperature: float):
+        if not isinstance(temperature, float) or not (temperature > 0):
+            raise ValueError(f"`temperature` has to be a strictly positive float, but is {temperature}")
 
+        self.temperature = temperature
+        self.mapping = {1: 10.0, 2: 9.53, 3: 9.06, 4: 8.59, 5: 8.12, 6: 7.65, 7: 7.18, 8: 6.71, 9: 6.24, 10: 5.77, 11: 5.30, 
+                        12: 4.83, 13: 4.36, 14: 3.89, 15: 3.42, 16: 2.95, 17: 2.49, 18: 2.01, 19: 1.54, 20: 1.0}
+
+    def __call__(self, input_ids: torch.Tensor, scores: torch.Tensor) -> torch.FloatTensor:
+        cur_len = input_ids.shape[-1]
+        self.temperature = self.mapping.get(cur_len, 1.0)
+        
+        return scores
+
+def calculate_perplexity_sliding(input_sentence, model, tokenizer, device, window_size=50):
+    """
+    Calculate min(exp(loss)) over a sliding window
+    """
+    tokenized = tokenizer(input_sentence)
+    input = torch.tensor(tokenized.input_ids).to(device)
+    min_perplexity = 100000
+    with torch.no_grad():
+        for start_idx in range(input.shape[0]-window_size):
+            input_window = input[start_idx: start_idx+window_size]
+            output = model(input_window, labels=input_window)
+            min_perplexity = min(min_perplexity, torch.exp(output.loss))
+    return min_perplexity
 
 
 def main(args):
@@ -22,7 +51,13 @@ def main(args):
     print("The sample of dataset is:", ds[:1000])
    
     seq_len = 256
-    top_k = 40
+    logits_warper = LogitsProcessorList(
+            [
+                DecayingTemperatureWarper(10.0)
+            ]
+        )
+    
+    # top_k = 1000
 
     print("Loading models...")
     
@@ -37,7 +72,7 @@ def main(args):
     
     samples = []
     prompts_list = []
-    scores = {"XL": [], "S": [], "Lower": [], "zlib": []}
+    scores = {"XL": [], "S": [], "Lower": [], "zlib": [], "window": []}
 
     num_batches = int(np.ceil(args.N / args.batch_size))
     
@@ -84,8 +119,10 @@ def main(args):
                 attention_mask=inputs['attention_mask'].to(device),
                 max_length=input_len + seq_len,
                 do_sample=True, 
-                top_k=top_k, 
-                top_p=1.0
+                # top_k=top_k, 
+                logits_processor = logits_warper,
+                renormalize_logits = True
+                # top_p=1.0
             )
 
             texts = tokenizer.batch_decode(output_sequences, skip_special_tokens=True)
@@ -95,7 +132,7 @@ def main(args):
                 p2 = calculate_perplexity(text, model2, tokenizer)
                 p_lower = calculate_perplexity(text.lower(), model1, tokenizer)
                 zlib_entropy = len(zlib.compress(bytes(text, 'utf-8')))
-
+                perplexity_window = calculate_perplexity_sliding(text.lower(), model1, tokenizer, device)
                 samples.append(text)
                 
                 
@@ -103,25 +140,42 @@ def main(args):
                 scores["S"].append(p2)
                 scores["Lower"].append(p_lower)
                 scores["zlib"].append(zlib_entropy)
-                
+                scores["window"].append(perplexity_window.cpu())
             pbar.update(args.batch_size)
-    print("*"*100)
-    print("Prompt List has the following prompts:",prompts_list[0])
+    # print("*"*100)
+    # print("Prompt List has the following prompts:",prompts_list[0])
     scores["XL"] = np.asarray(scores["XL"])
     scores["S"] = np.asarray(scores["S"])
     scores["Lower"] = np.asarray(scores["Lower"])
     scores["zlib"] = np.asarray(scores["zlib"])
-
+    scores["window"] = np.asarray(scores["window"])
     model1_name = args.model1.replace("/", "_")
     model2_name = args.model2.replace("/", "_")
     
+     # Remove duplicate samples
+    idxs = pd.Index(samples)
+    idxs_mask = ~(idxs.duplicated())
+    print(idxs_mask)
+    generated_samples_clean = np.asarray(samples)[idxs_mask]
+    generated_samples_clean = samples.tolist()
+    scores["XL"] = scores["XL"][idxs_mask]
+    scores["S"] = scores["S"][idxs_mask]
+    # scores["MEDIUM"] = scores["MEDIUM"][idxs_mask]
+    scores["Lower"] = scores["Lower"][idxs_mask]
+    scores["zlib"] = scores["zlib"][idxs_mask]
+    scores["window"] = scores["window"][idxs_mask]
+
+    assert len(generated_samples_clean) == len(scores["XL"])
+    assert len(scores["S"]) == len(scores["XL"])
+    print("Num duplicates:", len(samples) - len(generated_samples_clean))
+
     output_csv = f'output_scores_{model1_name}_{model2_name}.csv'
     with open(output_csv, 'w', newline='') as csvfile:
         fieldnames = ['sample', 'prompt', 'PPL_XL', 'PPL_S', 'PPL_Lower', 'Zlib']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        for sample,prompt, xl, s, lower, zlib_ in zip(samples, prompts_list[0], scores["XL"], scores["S"], scores["Lower"], scores["zlib"]):
-            writer.writerow({'sample': sample, 'prompt': prompt,'PPL_XL': xl, 'PPL_S': s, 'PPL_Lower': lower, 'Zlib': zlib_})
+        for sample,prompt, xl, s, lower, zlib_,window in zip(generated_samples_clean, prompts_list[0], scores["XL"], scores["S"], scores["Lower"], scores["zlib"], scores["window"]):
+            writer.writerow({'sample': sample, 'prompt': prompt,'PPL_XL': xl, 'PPL_S': s, 'PPL_Lower': lower, 'Zlib': zlib_, 'window': window})
 
     print("Results saved to ", output_csv)
 
@@ -129,23 +183,26 @@ def main(args):
     with open(output_txt, 'w') as f:
         metric = -np.log(scores["XL"])
         f.write(f"======== top sample by XL perplexity: ========\n")
-        f.write(print_best(metric, samples, "PPL", scores["XL"]))
+        f.write(print_best(metric, generated_samples_clean, "PPL", scores["XL"]))
         f.write("\n")
 
         metric = np.log(scores["S"]) / np.log(scores["XL"])
         f.write(f"======== top sample by ratio of S and XL perplexities: ========\n")
-        f.write(print_best(metric, samples, "PPL-XL", scores["XL"], "PPL-S", scores["S"]))
+        f.write(print_best(metric, generated_samples_clean, "PPL-XL", scores["XL"], "PPL-S", scores["S"]))
         f.write("\n")
 
         metric = np.log(scores["Lower"]) / np.log(scores["XL"])
         f.write(f"======== top sample by ratio of lower-case and normal-case perplexities: ========\n")
-        f.write(print_best(metric, samples, "PPL-XL", scores["XL"], "PPL-XL-Lower", scores["Lower"]))
+        f.write(print_best(metric, generated_samples_clean, "PPL-XL", scores["XL"], "PPL-XL-Lower", scores["Lower"]))
         f.write("\n")
 
         metric = scores["zlib"] / np.log(scores["XL"])
         f.write(f"======== top sample by ratio of Zlib entropy and XL perplexity: ========\n")
-        f.write(print_best(metric, samples, "PPL-XL", scores["XL"], "Zlib", scores["zlib"]))
+        f.write(print_best(metric, generated_samples_clean, "PPL-XL", scores["XL"], "Zlib", scores["zlib"]))
 
+        metric = scores["window"]
+        f.write(f"========  top samples by minimum XL perplexity across a sliding window of size 50: ========\n")
+        f.write(print_best(metric, generated_samples_clean, "PPL-Window", scores["window"]))
     print("Top results written to ", output_txt)
 
 def parse_arguments(argv):
