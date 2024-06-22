aae7153fe95fe347b88ab477795d223415a40914
Chaitanya Pandey <chaitanya.p2001@gmail.com>
2024-06-23 00:35:58 +0100

    pythia10kswa

--- cirus-scripts/pythia-swa-10k-6.9b.sh
+++ cirus-scripts/pythia-swa-10k-6.9b.sh
@@ -39,7 +39,7 @@ conda activate myenv
 pip install -r requirements.txt
 
 # Run the main script
-python main_load_input10000swa.py --N 10000 --batch-size 10 --model1 /work/tc062/tc062/s2605274/models/pythia-6.9b --model2 /work/tc062/tc062/s2605274/models/pythia-6.9b --corpus-path swa_sample.txt --name-tag pythia10k
+python main_load_input10000swa.py --N 10000 --batch-size 10 --model1 /work/tc062/tc062/s2605274/models/pythia-6.9b --model2 /work/tc062/tc062/s2605274/models/pythia-6.9b --corpus-path swa_sample.txt --name-tag pythia10kswa
 
 # Deactivate conda environment
 conda deactivate
