7e2f76b3228d4500c433699ab00ca8cc5c8b86ca
Chaitanya Pandey <chaitanya.p2001@gmail.com>
2024-06-25 00:03:25 +0100

    eddie scripts

---
+++ eddie_scripts/gpu-pythia-2.8fin100.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Use the variable for the job name and log/error files
+#$ -N Pythia-Fin-2.8b100
+#$ -o /exports/eddie/scratch/s2605274/job_runs/EDDIE-pythia-2.8b_$JOB_ID.log
+#$ -e /exports/eddie/scratch/s2605274/job_runs/EDDIE-pythia-2.8b_$JOB_ID.err
+#$ -cwd
+#$ -q gpu
+#$ -pe gpu 1
+#$ -l h_vmem=500G
+#$ -l h_rt=24:00:00
+#$ -m bea -M s2605274@ed.ac.uk 
+
+export HF_HOME="/work/tc062/tc062/s2605274/huggingface_cache"
+export TRANSFORMERS_CACHE="/work/tc062/tc062/s2605274/huggingface_cache/transformers"
+export HF_DATASETS_CACHE="/work/tc062/tc062/s2605274/huggingface_cache/datasets"
+
+source /work/tc062/tc062/s2605274/Miniconda3/etc/profile.d/conda.sh
+
+# Change to the working directory
+cd /work/tc062/tc062/s2605274/base_extraction_implementaion/
+
+# Activate conda environment
+conda activate myenv
+
+# Install required packages
+pip install -r requirements.txt
+
+# Run the main script
+python main_load_input10000finp.py --N 10000 --batch-size 10 --model1 EleutherAI/pythia-2.8b --model2 EleutherAI/pythia-2.8b --corpus-path fin_sample.txt --name-tag pythia2.8fin100
+
+# Deactivate conda environment
+conda deactivate
