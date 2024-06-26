1506f6826c6fb9496c68c5ee66103a0b2a2b9a4c
Chaitanya Pandey <chaitanya.p2001@gmail.com>
2024-06-26 22:50:23 +0100

    2.8

--- cirus-scripts/pythia-1.4fin50.sh
+++ cirus-scripts/pythia-1.4fin50.sh
@@ -37,7 +37,7 @@ conda activate myenv
 pip install -r requirements.txt
 
 # Run the main script
-python main_load_dp_fin.py --N 10000 --batch-size 10 --model1 /work/tc062/tc062/s2605274/models/pythia-2.8b --model2 /work/tc062/tc062/s2605274/models/pythia-2.8b --corpus-path fin_sample.txt --name-tag pythia1.4fin50
+python main_load_dp_fin.py --N 10000 --batch-size 10 --model1 /work/tc062/tc062/s2605274/models/pythia-2.8b --model2 /work/tc062/tc062/s2605274/models/pythia-2.8b --corpus-path fin_sample.txt --name-tag pythia2.8fin50
 
 # Deactivate conda environment
 conda deactivate
