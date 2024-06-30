3f9ab831625429d01596656f9a7939e4a2aae6ed
Chaitanya Pandey <chaitanya.p2001@gmail.com>
2024-06-30 18:37:06 +0100

    added a continue check

--- main_load_input10000swa.py
+++ main_load_input10000swa.py
@@ -86,6 +86,8 @@ def main(args):
                 token_ids= tokenized_chunk['input_ids'][0]
 
                 prompt_ids= token_ids[:input_len]
+                if prompt_ids.shape[0] < input_len:
+                    continue 
                 prompt= tokenizer.decode(prompt_ids, skip_special_tokens=True)
                 # print("the lenght of tokenized prompt is:", len(inputs))
                 # print(inputs)
