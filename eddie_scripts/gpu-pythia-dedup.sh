e6bb7b3475d3f0d0189dfefa6add42bcdb0e3756
Chaitanya Pandey <chaitanya.p2001@gmail.com>
2024-07-05 17:18:16 +0100

    dedup

--- eddie_scripts/gpu-pythia-dedup.sh
+++ eddie_scripts/gpu-pythia-dedup.sh
@@ -2,8 +2,8 @@
 
 # Use the variable for the job name and log/error files
 #$ -N Pythia-Swa-2.8_dedup
-#$ -o /exports/eddie/scratch/s2605274/job_runs/EDDIE-pythia-2.8b_$JOB_ID.log
-#$ -e /exports/eddie/scratch/s2605274/job_runs/EDDIE-pythia-2.8b_$JOB_ID.err
+#$ -o /exports/eddie/scratch/s2605274/job_runs/EDDIE-pythia-2.8bdedup_$JOB_ID.log
+#$ -e /exports/eddie/scratch/s2605274/job_runs/EDDIE-pythia-2.8bdedup_$JOB_ID.err
 #$ -cwd
 #$ -P lel_hcrc_cstr_students
 #$ -q gpu
