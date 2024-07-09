e5836f6ca97fe14f5754a3cad268478ac9092912
Chaitanya Pandey <chaitanya.p2001@gmail.com>
2024-07-10 00:42:14 +0100

    next run

--- eddie_scripts/gpu-pythia-quant.sh
+++ eddie_scripts/gpu-pythia-quant.sh
@@ -31,7 +31,7 @@ conda activate myenv
 pip install -r requirements.txt
 
 # Run the main script
-python main_load_input10000swa.py --N 10000 --batch-size 10 --model1 RichardErkhov/EleutherAI_-_pythia-2.8b-v0-8bits --model2 RichardErkhov/EleutherAI_-_pythia-2.8b-v0-8bits --corpus-path swa_sample.txt --name-tag pythiaswa2.8quant150_run2
+python main_load_input10000swa.py --N 10000 --batch-size 10 --model1 RichardErkhov/EleutherAI_-_pythia-2.8b-v0-8bits --model2 RichardErkhov/EleutherAI_-_pythia-2.8b-v0-8bits --corpus-path swa_sample.txt --name-tag pythiaswa2.8quant150_run3
 #python main.py --N 1000 --batch-size 10 --model1 EleutherAI/pythia-2.8b --model2 EleutherAI/pythia-410m --cor
 
 conda deactivate 
