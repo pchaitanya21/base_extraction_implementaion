798e1564fe40d8a69e35500e6e7fdc3f46579d8a
Chaitanya Pandey <chaitanya.p2001@gmail.com>
2024-07-12 14:48:20 +0100

    added tur run

--- eddie_scripts/gpu-pythia-2.8tur150.sh
+++ eddie_scripts/gpu-pythia-2.8tur150.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Use the variable for the job name and log/error files
-#$ -N Pythia-Fin-2.8b150
+#$ -N Pythia-Tur-1.4b150
 #$ -o /exports/eddie/scratch/s2605274/job_runs/EDDIE-pythia-2.8b_$JOB_ID.log
 #$ -e /exports/eddie/scratch/s2605274/job_runs/EDDIE-pythia-2.8b_$JOB_ID.err
 #$ -cwd
@@ -30,7 +30,7 @@ conda activate myenv
 pip install -r requirements.txt
 
 # Run the main script
-python main_load_input_fin.py --N 10000 --batch-size 10 --model1 EleutherAI/pythia-2.8b --model2 EleutherAI/pythia-2.8b --corpus-path fin_perplex.txt --name-tag pythia2.8bfin_baseline_run3_150
+python main_load_input_fin.py --N 10000 --batch-size 10 --model1 EleutherAI/pythia-2.8b --model2 EleutherAI/pythia-1.4b --corpus-path tur_sample.txt --name-tag pythia160mtur_run1_150
 
 # Deactivate conda environment
 conda deactivate
--- main_load_input_fin.py
+++ main_load_input_fin.py
@@ -57,7 +57,7 @@ def main(args):
     with tqdm(total=args.N) as pbar:
         for _ in range(num_batches):
             #input_len 25 works pile
-            input_len = 450 
+            input_len = 150 
             input_ids = []
             attention_mask = []
             
