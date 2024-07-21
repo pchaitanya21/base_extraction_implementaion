80d725ea95375420de28666beb217493389ca3ab
Chaitanya Pandey <chaitanya.p2001@gmail.com>
2024-07-21 20:49:24 +0100

    added dedup

---
+++ eddie_scripts/dedup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Use the variable for the job name and log/error files
+#$ -N Pythia-Swa-2.8b_quant_run2
+#$ -o /exports/eddie/scratch/s2605274/job_runs/EDDIE-pythia-2.8b_$JOB_ID.log
+#$ -e /exports/eddie/scratch/s2605274/job_runs/EDDIE-pythia-2.8b_$JOB_ID.err
+#$ -cwd
+#$ -P lel_hcrc_cstr_students
+#$ -q gpu
+#$ -pe gpu-a100 1
+#$ -l h_vmem=500G
+#$ -l h_rt=24:00:00
+#$ -m bea -M s2605274@ed.ac.uk 
+
+export HF_HOME="/exports/eddie/scratch/s2605274/.cache/huggingface_cache"
+export TRANSFORMERS_CACHE="/exports/eddie/scratch/s2605274/.cache/huggingface_cache/transformers"
+export HF_DATASETS_CACHE="/exports/eddie/scratch/s2605274/.cache/huggingface_cache/datasets"
+export PIP_CACHE_DIR="/exports/eddie/scratch/s2605274/.cache/pip"
+export CONDA_PKGS_DIRS="/exports/eddie/scratch/s2605274/.cache/conda_pkgs"
+
+source /exports/eddie/scratch/s2605274/miniconda3/etc/profile.d/conda.sh
+
+cd /exports/eddie/scratch/s2605274/base_extraction_implementaion/
+#conda remove --name extract --all
+python dedup.py
