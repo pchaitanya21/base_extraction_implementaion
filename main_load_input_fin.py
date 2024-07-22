8000347eabe790a197cb26eb7d3898d14705db91
Chaitanya Pandey <chaitanya.p2001@gmail.com>
2024-07-22 12:38:12 +0100

    fixed forced downlaod

--- main_load_input_fin.py
+++ main_load_input_fin.py
@@ -41,9 +41,9 @@ def main(args):
     # model2 = GPTNeoXForCausalLM.from_pretrained(args.model2, return_dict=True).to(device)
     # model2.eval()
     #for gpt-neo
-    model1 = AutoModelForCausalLM.from_pretrained(args.model1, return_dict=True).to(device)
+    model1 = AutoModelForCausalLM.from_pretrained(args.model1, return_dict=True, force_download=True).to(device)
     model1.config.pad_token_id = model1.config.eos_token_id
-    model2 = AutoModelForCausalLM.from_pretrained(args.model2, return_dict=True).to(device)
+    model2 = AutoModelForCausalLM.from_pretrained(args.model2, return_dict=True, force_download=True).to(device)
     model2.eval()
 
     samples = []
