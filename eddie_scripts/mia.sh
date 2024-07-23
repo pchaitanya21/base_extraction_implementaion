3a010daa2ed912fe99ca06e53ee92293a3a4b715
Chaitanya Pandey <chaitanya.p2001@gmail.com>
2024-07-23 09:15:47 +0100

    added new mia script

---
+++ eddie_scripts/mia.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Use the variable for the job name and log/error files
+#$ -N Pythia-mia
+#$ -o /exports/eddie/scratch/s2605274/job_runs/EDDIE-pythia-2.8b_$JOB_ID.log
+#$ -e /exports/eddie/scratch/s2605274/job_runs/EDDIE-pythia-2.8b_$JOB_ID.err
+#$ -cwd
+#$ -P lel_hcrc_cstr_students
+#$ -l h_vmem=500G
+#$ -l h_rt=24:00:00
+#$ -m bea -M s2605274@ed.ac.uk 
+
+export HF_HOME="/exports/eddie/scratch/s2605274/.cache/huggingface_cache"
+export TRANSFORMERS_CACHE="/exports/eddie/scratch/s2605274/.cache/huggingface_cache/transformers"
+export HF_DATASETS_CACHE="/exports/eddie/scratch/s2605274/.cache/huggingface_cache/datasets"
+export PIP_CACHE_DIR="/exports/eddie/scratch/s2605274/.cache/pip"
+export CONDA_PKGS_DIRS="/exports/eddie/scratch/s2605274/.cache/conda_pkgs"
+
+source /exports/eddie/scratch/s2605274/miniconda3/etc/profile.d/conda.sh
+
+cd /exports/eddie/scratch/s2605274/neighbour-mia/
+#conda remove --name extract --all
+conda activate myenv
+
+pip install -r requirements.txt
+
+python attack.py --proc-id 0 --model bert --dataset news
+
+conda deactivate
